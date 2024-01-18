b97baad2b59e70f94e1be79000c619e323ba1156
Prax007-dot <prakashvallal74@gmail.com>
2024-01-18 21:16:16 +0530

    day2 changes in form css for blur effect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+// const gap=()=>{
+//   return(
+    
+//   )
+// }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Signup Page</header>
+      <div id="container">
+        <h1>SignUp</h1>
+        <form id="form">
+          <div id="input1">
+            <label htmlFor='firstname'>Firstname: </label>
+            <input type='text' className='firstname' id="firstname" name='firstname' placeholder="First Name" required></input>
+          </div>
+          <div id="input1">
+            <label htmlFor='lastname'>Lastname: </label>
+            <input type='text' className='lastname' id="lastname" name='lastname' placeholder="Last Name" required></input>
+          </div>
+
+          <div id="input1">
+            <label htmlFor='email'>Email: </label>
+            <input type='email' className='email' id="email" name='email' placeholder="Email" required></input>
+          </div>
+
+          <div id="input1">
+            <label htmlFor='password'>Password: </label>
+            <input type='password' className='password' id="password" name='password' placeholder="Password" required></input>
+          </div>
+          
+          <button type='submit' className="button">Submit</button>
+        </form>
+      </div>
+      <footer>LInks</footer>
     </div>
   );
 }
