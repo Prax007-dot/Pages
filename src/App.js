9f41cf768ea649b5fdcc354cedd123da165d6b91
Prax007-dot <prakashvallal74@gmail.com>
2024-02-24 16:49:28 +0530

    sign up page is ready

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
 
 // const gap=()=>{
 //   return(
@@ -8,32 +10,69 @@ import './App.css';
 // }
 
 function App() {
+  const [formdata,setFormdata] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange=(e)=>{
+      const {name, value} = e.target;
+      
+      
+      setFormdata((prevData)=>{return{...prevData, [name]: value}})
+      console.log(formdata)
+  }
+  const sendData = async(e)=>{
+    e.preventDefault()
+    setFormdata({
+      firstname: '',
+    lastname: '',
+    email: '',
+    password: '',
+    })
+
+    try{
+      const res = await axios.post('http://localhost:5005/user', formdata)
+      console.log(res)
+      console.log(formdata)
+    
+    }catch(err){
+      console.log(err);
+      console.log("move on")
+      
+    }
+    
+  }
+
+
   return (
     <div className="App">
       <header>Signup Page</header>
       <div id="container">
         <h1>SignUp</h1>
-        <form id="form">
+        <form id="form" onSubmit={sendData}>
           <div id="input1">
             <label htmlFor='firstname'>Firstname: </label>
-            <input type='text' className='firstname' id="firstname" name='firstname' placeholder="First Name" required></input>
+            <input type='text' className='firstname' id="firstname" name='firstname' placeholder="First Name" onChange={(e)=>{handleChange(e)}}  required></input>
           </div>
           <div id="input1">
             <label htmlFor='lastname'>Lastname: </label>
-            <input type='text' className='lastname' id="lastname" name='lastname' placeholder="Last Name" required></input>
+            <input type='text' className='lastname' id="lastname" name='lastname' placeholder="Last Name" onChange={(e)=>{handleChange(e)}}  required></input>
           </div>
 
           <div id="input1">
             <label htmlFor='email'>Email: </label>
-            <input type='email' className='email' id="email" name='email' placeholder="Email" required></input>
+            <input type='email' className='email' id="email" name='email' placeholder="Email" onChange={(e)=>{handleChange(e)}}  required></input>
           </div>
 
           <div id="input1">
             <label htmlFor='password'>Password: </label>
-            <input type='password' className='password' id="password" name='password' placeholder="Password" required></input>
+            <input type='password' className='password' id="password" name='password' placeholder="Password" onChange={(e)=>{handleChange(e)}}  required></input>
           </div>
           
-          <button type='submit' className="button">Submit</button>
+          <button type='submit' className="button" >Submit</button>
         </form>
       </div>
       <footer>LInks</footer>
